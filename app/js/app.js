97fc0d8989e0ca236d9b8a1ca1283605e4e7da99
Melanie Burger <melanie.brgr@gmail.com>
2016-08-23 22:42:40 -0400

    Include Apli redesign, organize views

--- app/js/app.js
+++ app/js/app.js
@@ -4,22 +4,11 @@ var P1 = angular.module('portfolio', ['ngRoute']);
 
 P1.config(function($routeProvider) {
   $routeProvider
-    .when('/', {
-      templateUrl: 'js/home/home.html'
-    })
-    .when('/gctavatar', {
-      templateUrl: 'js/gctavatar/gctavatar.html'  	
-    })
-    .when('/symptomsleuth', {
-      templateUrl: 'js/symptomsleuth/symptomsleuth.html'      
-    })
-    .when('/shoppinglist', {
-      templateUrl: 'js/shoppinglist/shoppinglist.html'     
-    })
-    .when('/cocktailsquiz', {
-      templateUrl: 'js/cocktailsquiz/cocktailsquiz.html'   
-    })
-    .when('/lab3d', {
-      templateUrl: 'js/lab3d/lab3d.html'
-    });      
+    .when('/', {templateUrl: 'views/home/home.html'})
+    .when('/gctavatar', {templateUrl: 'views/gctavatar/gctavatar.html'})
+    .when('/symptomsleuth', {templateUrl: 'views/symptomsleuth/symptomsleuth.html'})
+    .when('/shoppinglist', {templateUrl: 'views/shoppinglist/shoppinglist.html'})
+    .when('/cocktailsquiz', {templateUrl: 'views/cocktailsquiz/cocktailsquiz.html'})
+    .when('/lab3d', {templateUrl: 'views/lab3d/lab3d.html'})
+    .when('/apliaredesign', {templateUrl: 'views/apliaredesign/apliaredesign.html'});  
 });
