94be854f25f7017ebb9cb9e5f2af818b1859204a
Melanie Burger <melanie.brgr@gmail.com>
2016-08-27 20:16:51 -0400

    Reorganize view directory and create content pages for all

--- app/js/app.js
+++ app/js/app.js
@@ -5,10 +5,20 @@ var P1 = angular.module('portfolio', ['ngRoute']);
 P1.config(function($routeProvider) {
   $routeProvider
     .when('/', {templateUrl: 'views/home/home.html'})
-    .when('/gctavatar', {templateUrl: 'views/gctavatar/gctavatar.html'})
-    .when('/symptomsleuth', {templateUrl: 'views/symptomsleuth/symptomsleuth.html'})
-    .when('/shoppinglist', {templateUrl: 'views/shoppinglist/shoppinglist.html'})
-    .when('/cocktailsquiz', {templateUrl: 'views/cocktailsquiz/cocktailsquiz.html'})
-    .when('/lab3d', {templateUrl: 'views/lab3d/lab3d.html'})
-    .when('/apliaredesign', {templateUrl: 'views/apliaredesign/apliaredesign.html'});  
+    // Front-End Web Development
+    .when('/gctavatar', {templateUrl: 'views/frontendwebdevelopment/gctavatar/gctavatar.html'})
+    .when('/symptomsleuth', {templateUrl: 'views/frontendwebdevelopment/symptomsleuth/symptomsleuth.html'})
+    .when('/shoppinglist', {templateUrl: 'views/frontendwebdevelopment/shoppinglist/shoppinglist.html'})
+    .when('/cocktailsquiz', {templateUrl: 'views/frontendwebdevelopment/cocktailsquiz/cocktailsquiz.html'})
+    .when('/lab3d', {templateUrl: 'views/frontendwebdevelopment/lab3d/lab3d.html'})
+    // Design & Visual Communication
+    .when('/apliaredesign', {templateUrl: 'views/designandvisualcommmunication/apliaredesign/apliaredesign.html'})
+    .when('/cerebralbloodflow', {templateUrl: 'views/designandvisualcommmunication/cerebralbloodflow/cerebralbloodflow.html'})
+    .when('/externalvisualfield', {templateUrl: 'views/designandvisualcommmunication/externalvisualfield/externalvisualfield.html'})
+    .when('/naturecover', {templateUrl: 'views/designandvisualcommmunication/naturecover/naturecover.html'})
+    // Other
+    .when('/writingandspeaking', {templateUrl: 'views/writingandspeaking/writingandspeaking.html'})
+    .when('/readingnotes', {templateUrl: 'views/readingnotes/readingnotes.html'})
+    .when('/experiments', {templateUrl: 'views/experiments/experiments.html'})
+    .otherwise({redirectTo: '/'});
 });
